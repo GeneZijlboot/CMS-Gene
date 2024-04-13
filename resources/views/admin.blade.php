4cc42e9050cec218347ad027519d593de413e032
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2024-04-13 19:14:38 +0200

    setting up laravel with sass

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- include sass -->
+    @vite(['resources/scss/admin.scss'])
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="login-wrapper">
+        <h1>kjsndf</h1>
+        oksandiljgnhjasngjlasnjk    
+        ]sofmgoasm;\ias;
+        oifndgam
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">aokgnadf</div>
+            <div class="col-md-6">sag</div>
+        </div>
+    </div>
+</body>
+</html>
